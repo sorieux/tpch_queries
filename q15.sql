993b89ef3f91ff563ba086ce63a0d22baad553b9
sorieux <orieux.stephane@gmail.com>
2022-10-16 15:48:04 +0200

    Update q15.sql

--- q15.sql
+++ q15.sql
@@ -28,4 +28,37 @@ WHERE
 		revenue
   )
 ORDER BY
-	s.suppkey;
+	s.suppkey
+	
+	
+WITH revenue AS (
+	SELECT
+			l.suppkey AS supplier_no,
+			sum(l.extendedprice * (1 - l.discount)) AS total_revenue
+	FROM
+			lineitem l
+	WHERE
+			l.shipdate >= DATE '1996-01-01'
+		AND l.shipdate < DATE '1996-01-01' + INTERVAL '3' MONTH
+	GROUP BY
+			l.suppkey
+)
+SELECT
+	s.suppkey,
+	s.name,
+	s.address,
+	s.phone,
+	total_revenue
+FROM
+	supplier s,
+	revenue
+WHERE
+	s.suppkey = supplier_no
+	AND total_revenue = (
+	SELECT
+		max(total_revenue)
+	FROM
+		revenue
+  )
+ORDER BY
+	s.suppkey
